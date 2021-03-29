97814a8c65cae7dfef2b1569e3ac2e0a7f20a178
Samkishline <Samkishline@gmail.com>
2021-03-28 21:20:03 -0700

    Final functionality implemented

--- sketch.js
+++ sketch.js
@@ -39,10 +39,26 @@ let phoneMiddle = ((phoneLeft + phoneRight) / 2);
 var img;
 var imgLoaded;
 
+//song bpm
+let bpm;
+
+//music wave function variables
+let xspacing = 20; // Distance between each horizontal location
+let w; // Width of entire wave
+let theta = 0; // Start angle at 0
+let amplitude = 75.0; // Height of wave
+let period = 400.0; // How many pixels before the wave repeats
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+
+let mappedBPM = 0.05;
+
 //timer to load and initialize player
 let loaded = false;
 
 let drawButtonsOnce = true;
+
+let isPlaying = false;
 
 const sampleAndPlayForever = () => {
     player.stop();
@@ -57,6 +73,9 @@ const sampleAndPlayForever = () => {
 const changeTempo = (delta) => {
     tempo = Math.max(Math.min(tempo + delta * 10, 120), 40);
     //display Tempo
+
+    //set mapped bpm
+    Bpm(round(tempo));
 }
 
 const start = () => {
@@ -64,11 +83,13 @@ const start = () => {
     changeTempo(0);
     stopSignal = false;
     sampleAndPlayForever();
+    isPlaying = true;
 };
 
 const stop = () => {
     stopSignal = true;
     player.stop();
+    isPlaying = false;
 };
 
 model.initialize().then(stop);
@@ -106,6 +127,14 @@ function setup() {
 
     //hearbeat rad
     radius = 75;
+
+    //bpm initialize
+    bpm = 75;
+
+    //music wave setup
+    w = (phoneRight - phoneLeft) + 16;
+    dx = (TWO_PI / period) * xspacing;
+    yvalues = new Array(floor(w / xspacing));
 }
 
 function startMusic() {
@@ -142,6 +171,7 @@ function draw() {
         image(imgLoaded, 0, 0);
 
         if (drawButtonsOnce) {
+            //load buttons to bottom bar on iphone
             createButtons();
             drawButtonsOnce = false;
         }
@@ -150,35 +180,39 @@ function draw() {
 
         SetHeartRate();
 
-        push();
-        textSize(100);
-        fill(0);
-        text(round(tempo), phoneLeft + 210, phoneTop + 160)
-        pop();
+        drawHeartRate();
+
+        if (isPlaying) {
+            musicWave();
+        }
+
+
     } else {
         image(img, 0, 0)
 
         loadBar();
 
     }
-    //load buttons to bottom bar on iphone
-
-
-    //startup the music
-
 
     if (isChangeable) {
         //potentially change BPM
     }
 
-    //    run map function
-    //    run Bpm function
+}
+
+function musicWave() {
+
+    calcWave();
+    renderWave();
 
 }
 
-function mapHeartRate() {
-
-    //heartRate = map function
+function drawHeartRate() {
+    push();
+    textSize(100);
+    fill(0);
+    text(round(tempo), phoneLeft + 210, phoneTop + 160)
+    pop();
 }
 
 function heartBeat() {
@@ -272,52 +306,123 @@ function LoadProgram() {
 
 }
 
-function Bpm() {
+function Bpm(x) {
     //x allows us to create a case that isn't a single integer but rather a comparison between numbers.
-    var x = heartRate;
+
     switch (true) {
+        case (x >= 40 && x <= 49):
+            //HR - 50-75
+            console.log("1");
+
+            mappedBPM = map(x, 40, 49, 0.05, 0.06);
+            console.log(mappedBPM);
+            break;
         case (x >= 50 && x <= 75):
             //HR - 50-75
+            console.log("2");
 
+            mappedBPM = map(x, 50, 75, 0.06, 0.07);
+            console.log(mappedBPM);
             break;
         case (x >= 76 && x <= 85):
             //HR - 76-85
+            console.log("3");
 
+            mappedBPM = map(x, 76, 85, 0.07, 0.08);
+            console.log(mappedBPM);
             break;
         case (x >= 86 && x <= 95):
             //HR - 86-95
+            console.log("4");
 
+            mappedBPM = map(x, 86, 95, 0.08, 0.09);
+            console.log(mappedBPM);
             break;
         case (x >= 96 && x <= 100):
             //HR - 96-100
+            console.log("5");
 
+            mappedBPM = map(x, 96, 100, 0.09, 0.1);
+            console.log(mappedBPM);
             break;
         case (x >= 101 && x <= 105):
             //HR - 101-105
+            console.log("6");
+
+            mappedBPM = map(x, 101, 105, 0.1, 0.11);
+            console.log(mappedBPM);
             break;
         case (x >= 106 && x <= 110):
             //HR - 106-110
+            console.log("7");
+
+            mappedBPM = map(x, 106, 110, 0.11, 0.12);
+            console.log(mappedBPM);
             break;
         case (x >= 111 && x <= 120):
             //HR - 111-120
+            console.log("8");
+
+            mappedBPM = map(x, 111, 120, 0.12, 0.13);
+            console.log(mappedBPM);
             break;
         case (x >= 121 && x <= 150):
             //HR - 121-150
+            console.log("9");
+
+            mappedBPM = map(x, 121, 150, 0.13, 0.14);
+            console.log(mappedBPM);
             break;
         case (x >= 151 && x <= 175):
             //HR - 151-175
+            console.log("10");
+
+            mappedBPM = map(x, 151, 175, 0.14, 0.15);
+            console.log(mappedBPM);
             break;
         case (x >= 176 && x <= 200):
             //HR - 176-200+
+            console.log("11");
+
+            mappedBPM = map(x, 176, 200, 0.15, 0.16);
+            console.log(mappedBPM);
             break;
         case (x > 200):
             //HR - 200+
+            console.log("12");
+
+            mappedBPM = 999;
+            console.log(mappedBPM);
             //Out of range below or above - let's play something based on last heart rate or an intro song
             break;
         default:
             break;
     }
 
+}
+
+//sine wave derived from https://p5js.org/examples/math-sine-wave.html
+function calcWave() {
+    // Increment theta (try different values for
+    // 'angular velocity' here)
+
+    theta += mappedBPM;
+
+    // For every x value, calculate a y value with sine function
+    let x = theta;
+    for (let i = 0; i < yvalues.length; i++) {
+        yvalues[i] = sin(x) * amplitude;
+        x += dx;
+    }
+}
+
+function renderWave() {
+    noStroke();
+    fill(0);
+    // A simple way to draw the wave with an ellipse at each location
+    for (let x = 0; x < yvalues.length; x++) {
+        ellipse(x * xspacing + phoneLeft - 3, height / 2 + yvalues[x], 16, 16);
+    }
 }
 
 function DecideSongChange() {
